f5c80aefa651e1b1cb5ad67e89fdd500d6e40fcb
Luis Tapia <luisdamian.tapiaguerrero@gmail.com>
2020-09-07 20:47:06 -0500

    Initial commit

---
+++ src/main/java/com/luistapia/rest/api/elasticsearch/RestApiElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.luistapia.rest.api.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luistapia/rest/api/elasticsearch/RestApiElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luistapia.rest.api.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
